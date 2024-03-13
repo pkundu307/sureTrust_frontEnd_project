f355c4d58aa5fa5d29be156cb36e895b5f822101
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2024-03-13 16:53:02 +0530

    header addes

---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+ 
+      </Router>
+    </>
+  )
+}
+
+export default Pages
